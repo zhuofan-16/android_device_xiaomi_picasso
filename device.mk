fe2768e1b520cabd30ea95be5a371c48d5c940a3
Mango <mango@gmail.com>
2020-10-08 07:21:56 +0300

    picasso:device -nuke boot jars for now

--- device.mk
+++ device.mk
@@ -151,8 +151,8 @@ PRODUCT_PACKAGES += \
     qti_telephony_utils.xml \
     telephony-ext 
 
-PRODUCT_BOOT_JARS += \
-    telephony-ext
+#PRODUCT_BOOT_JARS += \
+#    telephony-ext
 
 # WiFi Display
 PRODUCT_PACKAGES += \
