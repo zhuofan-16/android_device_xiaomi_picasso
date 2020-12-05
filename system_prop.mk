1db1d0535b0cbda61fb9732d17d8118eef8696b5
masemoel <az.az.az.mjme@gmail.com>
2020-12-05 12:27:46 +0100

    picasso: Update ART props

--- system_prop.mk
+++ system_prop.mk
@@ -1,13 +1,12 @@
 # ART
 PRODUCT_PRODUCT_PROPERTIES += \
-    pm.dexopt.ab-ota=speed-profile \
+    dalvik.vm.dex2oat64.enabled=true \
     dalvik.vm.dex2oat-filter=speed \
     dalvik.vm.image-dex2oat-filter=speed \
     ro.vendor.qti.am.reschedule_service=true \
     ro.sys.fw.dex2oat_thread_count=8 \
     dalvik.vm.boot-dex2oat-threads=8 \
-    dalvik.vm.dex2oat-threads=4 \
-    dalvik.vm.image-dex2oat-threads=4
+    dalvik.vm.dex2oat-threads=8
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
