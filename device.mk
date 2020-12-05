692241d36220ebebc746cf016d16df6f645aa5f1
masemoel <az.az.az.mjme@gmail.com>
2020-12-05 12:32:48 +0100

    picasso: Add pixel hardware soong namespace

--- device.mk
+++ device.mk
@@ -66,6 +66,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/pixel \
     vendor/qcom/opensource/commonsys/packages/apps/Bluetooth \
     vendor/qcom/opensource/commonsys/system/bt/conf
     
