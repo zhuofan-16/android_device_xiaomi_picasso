fe9b2e621eeebad5368b0851bbb5527bddb92aeb
masemoel <az.az.az.mjme@gmail.com>
2020-12-10 12:48:06 +0100

    picasso: Touches II

--- device.mk
+++ device.mk
@@ -176,6 +176,7 @@ PRODUCT_PACKAGES += \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
     qti_telephony_utils.xml \
+    telephony-ext 
 
 PRODUCT_BOOT_JARS += \
     telephony-ext
