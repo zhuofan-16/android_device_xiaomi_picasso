c57eeb459bd1c7600793397c40fb769683a9eab3
masemoel <az.az.az.mjme@gmail.com>
2021-01-04 23:14:56 +0100

    picasso: Fix calls

--- device.mk
+++ device.mk
@@ -11,7 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # ANXCamera
-$(call inherit-product-if-exists, vendor/ANXCamera/config.mk)
+$(call inherit-product-if-exists, vendor/aeonax/ANXCamera/anx-vendor.mk)
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
