da7256c7e127e75c78b3730134d7af3ff7b8e223
ZhuoFan <4582h@dunearn.edu.sg>
2021-02-07 18:28:20 +0800

    Revert "2021 New Year Update"
    This reverts commit 52a75a41f5c8338969d6556246dc638765b29497.
    
    Revert 52a75a41f5c8338969d6556246dc638765b29497

--- rr_picasso.mk
+++ rr_picasso.mk
@@ -3,8 +3,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-#ANX CAMERA
-$(call inherit-product, vendor/ANXCamera/config.mk)
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
